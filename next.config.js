8471687420692dd6405eac63b95f8b021ef566bb
rishabhrathod01 <rathod.rishabh@prophecy.io>
2025-11-26 13:06:13 +0530

    Configure for GitHub Pages static export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   images: {
+    unoptimized: true,
     domains: ['images.unsplash.com', 'github.com'],
   },
 }
